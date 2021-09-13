882df009e4efec8a976d340d3bcb1a057c860ed3
safwen-code <safwendjebbi1234@gmail.com>
2021-09-13 16:21:18 +0100

    this project is deployed in netlify

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 import "./App.css";
 import React from "react";
 import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbars from "./Components/Navbars";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
+import Navbars from "./Components/Navbars";
 import AddBooks from "./Components/AddBooks";
 import useLocalStorages from "./Hooks/useLocalStorages";
 import Edite from "./Components/Edite";
 import BooksList from "./Components/BookList";
+
+import BooksContext from "./Context/BooksContext";
+
 const App = () => {
   const [books, setbooks] = useLocalStorages("books", []);
   return (
     <Router>
       <Navbars />
-      <Switch>
-        <Container>
-          <Route
-            render={(props) => (
-              <BooksList {...props} books={books} setbooks={setbooks}s />
-            )}
-            path="/"
-            exact={true}
-          />
-          <Route
-            path="/addDoc"
-            render={(props) => (
-              <AddBooks {...props} books={books} setbooks={setbooks} />
-            )}
-          />
-          <Route
-            path="/Edite/:id"
-            render={(props) => (
-              <Edite {...props} books={books} setbooks={setbooks} />
-            )}
-          />
-        </Container>
-      </Switch>
+      <BooksContext.Provider value={{ books, setbooks }}>
+        <Switch>
+          <Container>
+            <Route path="/" exact={true} component={BooksList} />
+            <Route path="/addDoc" component={AddBooks} />
+            <Route path="/Edite/:id" component={Edite} />
+            <Route component={() => <Redirect to="/" />} />
+          </Container>
+        </Switch>
+      </BooksContext.Provider>
     </Router>
   );
 };
--- src/Hooks/useLocalStorages.js
+++ src/Hooks/useLocalStorages.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const useLocalStorages = (key, initialValue) => {
   const [value, setValue] = useState(() => {
